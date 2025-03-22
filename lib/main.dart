ffa49a09336cbf270b94b10aba857aa32106cc19
Pushpraj-MTT <mediatechtemple.coder6@gmail.com>
2025-03-22 12:05:01 +0530

    nitial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:indiasarkarinaukri/themes/dark_theme.dart';
 import 'package:indiasarkarinaukri/themes/light_theme.dart';
+import 'package:indiasarkarinaukri/utils/notification_page.dart';
+import 'package:indiasarkarinaukri/viewmodels/theme_viewmodel.dart';
 import 'package:indiasarkarinaukri/views/HomeScreen.dart';
+import 'package:indiasarkarinaukri/views/mocktest.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-import 'viewmodels/theme_viewmodel.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();  // Initialize Firebase
+
+  // Enable Crashlytics collection
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  // FirebaseCrashlytics.instance.crash();
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    print('Message clicked!');
+    // Handle the notification click (e.g., navigate to a specific screen)
+  });
   runApp( MyApp());
 }
 
@@ -14,37 +33,31 @@ class MyApp extends StatelessWidget {
   MyApp({super.key});
   final ThemeViewModel themeViewModel = Get.put(ThemeViewModel());
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Obx(()=>GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: themeViewModel.isDarkMode.value ? darkTheme : lightTheme,
-      home: Homescreen(),
-    ));
+    return Obx(() {
+      return GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: themeViewModel.isDarkMode.value ? darkTheme : lightTheme,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const Homescreen(),
+          //'/mock_test': (context) => MockTestPage(),
+          '/notification': (context) => NotificationPage(),
+        },
+      );
+    });
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
+// Home Page (Main Screen)
+class HomePage extends StatelessWidget {
+  const HomePage({super.key}); // Added key parameter
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
 
     );
--- lib/views/HomeScreen.dart
+++ lib/views/HomeScreen.dart
@@ -4,9 +4,24 @@ import 'package:indiasarkarinaukri/views/Drawer.dart';
 import 'package:indiasarkarinaukri/views/UIHelper.dart';
 
 import '../utils/ScreenUtils.dart';
+import '../utils/notification_page.dart';
 
-class Homescreen extends StatelessWidget {
+class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
+
+  @override
+  _HomescreenState createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  // Default selected index for BottomNavigationBar
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,26 +29,41 @@ class Homescreen extends StatelessWidget {
 
     double screenWidth = ScreenUtils.getScreenWidth(context);
     double screenHeight = ScreenUtils.getScreenHeight(context);
+
     return Scaffold(
       appBar: AppBar(
-        title:   UIHelper.textview("India sarkari Naukri", FontWeight.bold, 20, GoogleFonts.poppins(), Colors.black),
-
+        title: UIHelper.textview(
+          "India Sarkari Naukri",
+          FontWeight.bold,
+          20,
+          GoogleFonts.poppins(),
+          Colors.black,
+        ),
         elevation: 4,
         actions: [
           Row(
             children: [
-              Material(
-                shape: CircleBorder(),
-                elevation: 4,
-                child: CircleAvatar(
-                  backgroundColor:
-                      Theme.of(context).brightness == Brightness.dark
-                          ? Colors
-                              .black54 // Dark background for dark mode
-                          : Colors.white, // White background for light mode,
-                  child: Icon(
-                    Icons.notifications_none,
-                    color: Theme.of(context).iconTheme.color,
+              // Notification Icon: Navigate to Notification Page
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const NotificationPage(), // Navigate to Notification Page
+                    ),
+                  );
+                },
+                child: Material(
+                  shape: CircleBorder(),
+                  elevation: 4,
+                  child: CircleAvatar(
+                    backgroundColor: Theme.of(context).brightness == Brightness.dark
+                        ? Colors.black54
+                        : Colors.white,
+                    child: Icon(
+                      Icons.notifications_none,
+                      color: Theme.of(context).iconTheme.color,
+                    ),
                   ),
                 ),
               ),
@@ -42,18 +72,16 @@ class Homescreen extends StatelessWidget {
                 shape: CircleBorder(),
                 elevation: 4,
                 child: CircleAvatar(
-                  backgroundColor:
-                      Theme.of(context).brightness == Brightness.dark
-                          ? Colors
-                              .black54 // Dark background for dark mode
-                          : Colors.white, // White background for light mode
+                  backgroundColor: Theme.of(context).brightness == Brightness.dark
+                      ? Colors.black54
+                      : Colors.white,
                   child: Icon(
                     Icons.person_outline,
                     color: Theme.of(context).iconTheme.color,
                   ),
                 ),
               ),
-              SizedBox(width: screenWidth*0.02),
+              SizedBox(width: screenWidth * 0.02),
             ],
           ),
         ],
@@ -68,54 +96,113 @@ class Homescreen extends StatelessWidget {
                 padding: EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
                 child: UIHelper.searchView(c),
               ),
-        
-              Row(children: [
-                UIHelper.textview("Trending", FontWeight.bold, 18, GoogleFonts.poppins(), Colors.black),
-                Spacer(),
-                UIHelper.textview("View All", FontWeight.bold, 18, GoogleFonts.poppins(), Colors.black),
-              ],),
-        
-              SizedBox(height: screenHeight * 0.01,),
-        
+              Row(
+                children: [
+                  UIHelper.textview("Trending", FontWeight.bold, 18, GoogleFonts.poppins(), Colors.black),
+                  Spacer(),
+                  UIHelper.textview("View All", FontWeight.bold, 18, GoogleFonts.poppins(), Colors.black),
+                ],
+              ),
+              SizedBox(height: screenHeight * 0.01),
               UIHelper.slider(),
-        
-              SizedBox(height: screenHeight * 0.02,),
-        
-              UIHelper.cardviewwithlist(title: "Latest Jobs",   items: [
-                        "UPSC Civil Services IAS / IFS Pre Online Form 2025",
-                        "CISF Constable Driver Online Form 2025",
-                        "Coast Guard Navik GD, DB CGEPT 02/2025 Online",
-                        "Railway RRB Group D Online Form 2025",
-                        "Bihar Panchayati Raj Gram Katchahary Sachiv Online",
-                      ], color: Colors.red),
-              SizedBox(height: screenHeight * 0.02,),
-              UIHelper.cardviewwithlist(title: "Result",   items: [
-                        "UPSC Civil Services IAS / IFS Pre Online Form 2025",
-                        "CISF Constable Driver Online Form 2025",
-                        "Coast Guard Navik GD, DB CGEPT 02/2025 Online",
-                        "Railway RRB Group D Online Form 2025",
-                        "Bihar Panchayati Raj Gram Katchahary Sachiv Online",
-                      ], color: Colors.green),
-              SizedBox(height: screenHeight * 0.02,),
-              UIHelper.cardviewwithlist(title: "Admit Card",   items: [
-                        "UPSC Civil Services IAS / IFS Pre Online Form 2025",
-                        "CISF Constable Driver Online Form 2025",
-                        "Coast Guard Navik GD, DB CGEPT 02/2025 Online",
-                        "Railway RRB Group D Online Form 2025",
-                        "Bihar Panchayati Raj Gram Katchahary Sachiv Online",
-                      ], color: Colors.green),
-              SizedBox(height: screenHeight * 0.02,),
-              UIHelper.cardviewwithlist(title: "Admission",   items: [
-                        "UPSC Civil Services IAS / IFS Pre Online Form 2025",
-                        "CISF Constable Driver Online Form 2025",
-                        "Coast Guard Navik GD, DB CGEPT 02/2025 Online",
-                        "Railway RRB Group D Online Form 2025",
-                        "Bihar Panchayati Raj Gram Katchahary Sachiv Online",
-                      ], color: Colors.red),
-        
+              SizedBox(height: screenHeight * 0.02),
+              UIHelper.cardviewwithlist(
+                title: "Latest Jobs",
+                items: [
+                  "UPSC Civil Services IAS / IFS Pre Online Form 2025",
+                  "CISF Constable Driver Online Form 2025",
+                  "Coast Guard Navik GD, DB CGEPT 02/2025 Online",
+                  "Railway RRB Group D Online Form 2025",
+                  "Bihar Panchayati Raj Gram Katchahary Sachiv Online",
+                ],
+                color: Colors.red,
+              ),
+              SizedBox(height: screenHeight * 0.02),
+              UIHelper.cardviewwithlist(
+                title: "Result",
+                items: [
+                  "UPSC Civil Services IAS / IFS Pre Online Form 2025",
+                  "CISF Constable Driver Online Form 2025",
+                  "Coast Guard Navik GD, DB CGEPT 02/2025 Online",
+                  "Railway RRB Group D Online Form 2025",
+                  "Bihar Panchayati Raj Gram Katchahary Sachiv Online",
+                ],
+                color: Colors.green,
+              ),
+              SizedBox(height: screenHeight * 0.02),
+              UIHelper.cardviewwithlist(
+                title: "Admit Card",
+                items: [
+                  "UPSC Civil Services IAS / IFS Pre Online Form 2025",
+                  "CISF Constable Driver Online Form 2025",
+                  "Coast Guard Navik GD, DB CGEPT 02/2025 Online",
+                  "Railway RRB Group D Online Form 2025",
+                  "Bihar Panchayati Raj Gram Katchahary Sachiv Online",
+                ],
+                color: Colors.green,
+              ),
+              SizedBox(height: screenHeight * 0.02),
+              UIHelper.cardviewwithlist(
+                title: "Admission",
+                items: [
+                  "UPSC Civil Services IAS / IFS Pre Online Form 2025",
+                  "CISF Constable Driver Online Form 2025",
+                  "Coast Guard Navik GD, DB CGEPT 02/2025 Online",
+                  "Railway RRB Group D Online Form 2025",
+                  "Bihar Panchayati Raj Gram Katchahary Sachiv Online",
+                ],
+                color: Colors.red,
+              ),
+              SizedBox(height: screenHeight * 0.02),
+
+              // Advertisement With Us Section
+              GestureDetector(
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  color: Colors.blue, // Adjust the color as needed
+                  child: Center(
+                    child: UIHelper.textview(
+                      "Advertisement With Us",
+                      FontWeight.bold,
+                      18,
+                      GoogleFonts.poppins(),
+                      Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
+      ),
+      // Bottom Navigation Bar with black icons
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.black, // Selected item color black
+        unselectedItemColor: Colors.black, // Unselected item color black
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_border),
+            label: 'Saved',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment_rounded),
+            label: 'MockTest',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.work),
+            label: 'Jobs',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: 'Library',
+          ),
+        ],
       ),
     );
   }
