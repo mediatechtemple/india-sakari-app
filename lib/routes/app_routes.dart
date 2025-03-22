3d05de2a1aeee38acf07584873c05ee4937c6ea8
Pushpraj-MTT <mediatechtemple.coder6@gmail.com>
2025-03-22 17:00:46 +0530

    Add new functionality

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,6 +3,7 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import '../views/advertise_with_us.dart';
 import '../utils/notification_page.dart';
 import '../views/HomeScreen.dart';
+import '../views/library.dart';
 import '../views/mocktest.dart';
 
 class AppRoutes {
@@ -10,6 +11,7 @@ class AppRoutes {
   static const String mockTest = '/mock_test';
   static const String notification = '/notification';
   static const String advertisementpage = '/advertisementwithus';
+  static const String librarypage = '/library';
 
 
   static List<GetPage> routes = [
@@ -17,5 +19,6 @@ class AppRoutes {
     GetPage(name: mockTest, page: () => MockTestPage()),
     GetPage(name: notification, page: () => NotificationPage()),
     GetPage(name: advertisementpage, page: () => AdvertiseWithUsPage()),
+    GetPage(name: librarypage, page: () => LibraryPage()),
   ];
 }
